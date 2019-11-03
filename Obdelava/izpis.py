c08d3bc51438fbb414a430a2398a1f869b714b08
Podborsek <gasper.rotar@student.fmf.uni-lj.si>
2019-11-03 20:37:20 +0100

    Zbral podatke

--- Obdelava/izpis.py
+++ Obdelava/izpis.py
@@ -3,10 +3,8 @@ import re
 import requests
 import orodja
 
-with open("igra_2.html") as dat:
-    stran = dat.read()
 
-izraz = re.compile(
+glavni_vzorec = re.compile(
     r'<meta name=\"title\" content=\"(?P<ime>.+?)\">.+?'
     r'{\"gameid\":\[\"(?P<id>.+?)\"\],.+?'
     r'\"yearpublished\":\"(?P<leto>\d+?)\",'
@@ -15,12 +13,53 @@ izraz = re.compile(
     r'\"minplaytime\":\"(?P<mintime>\d+?)\",'
     r'\"maxplaytime\":\"(?P<maxtime>\d+?)\",'
     r'\"minage\":\"(?P<minage>\d+?)\".+?'
-    #r'\"boardgamecategory\":\[(.+?)\].+?'
-    #r'\"boardgamemechanic\":\[(.+?)\].+?'
     r'\"ratingValue\": \"(?P<ocena>.+?)\".+?'
     r'\"reviewCount\": \"(?P<glasovi>\d+?)\"',
     flags=re.DOTALL
 )
 
-print(izraz.search(stran).groupdict())
+kategorije_blok = re.compile(
+    r'\"boardgamecategory\":\[(.+?)\].+?',
+    flags=re.DOTALL
+)
 
+mechanic_blok = re.compile(
+    r'\"boardgamemechanic\":\[(.+?)\].+?',
+    flags=re.DOTALL
+)
+
+km_vzorec = re.compile(
+    r'\"name\":\"(.+?)\"',
+    flags=re.DOTALL
+)
+
+'''
+problemi = []
+for i in range(1,100):
+    print(i)
+    try:
+        with open("igra_{}.html".format(i), encoding='utf-8') as dat:
+            stran = dat.read()
+        print(glavni_vzorec.search(stran).groupdict())
+        print(re.findall(km_vzorec, re.findall(kategorije_blok, stran)[0]))
+        print(re.findall(km_vzorec,re.findall(mechanic_blok, stran)[0]))
+    except:
+        print("Napaka")
+        problemi.append(i)
+
+print(problemi)
+'''
+
+podatki = []
+for i in range(1,501):
+    try:
+        with open("igra_{}.html".format(i), encoding='utf-8') as dat:
+            stran = dat.read()
+        slovar = glavni_vzorec.search(stran).groupdict()
+        slovar['kategorije'] = re.findall(km_vzorec, re.findall(kategorije_blok, stran)[0])
+        slovar['mechanic'] = re.findall(km_vzorec,re.findall(mechanic_blok, stran)[0])
+        podatki.append(slovar)
+    except:
+        print("Napaka pri primeru {}".format(i))
+
+orodja.zapisi_csv(podatki, ['ime', 'id', 'leto', 'minplay', 'maxplay', 'mintime', 'maxtime', 'minage', 'ocena', 'glasovi', 'kategorije', 'mechanic'], "test.csv")
--- zajem.py
+++ zajem.py
@@ -7,8 +7,9 @@ import orodja
 #r"<div id='results_objectname\d+' style='z-index:1000;' onclick=''>.*?<a  href=\"(?P<href>\/boardgame\/(?P<id>\d+?)\/.*?)\"   >(?P<ime>.*?)<\/a>"
 
 # Najprej moramo iz prvih petih strani dobiti seznam najpopularnejsih iger:
-#for st in range(1,6):
-#    orodja.shrani_spletno_stran("https://boardgamegeek.com/browse/boardgame/page/{}?sort=numvoters&sortdir=desc".format(st), "najigre_stran_{}".format(st))
+'''
+for st in range(1,11):
+    orodja.shrani_spletno_stran("https://boardgamegeek.com/browse/boardgame/page/{}?sort=numvoters&sortdir=desc".format(st), "najigre_stran_{}.html".format(st))
 
 vzorec_za_igre = re.compile(
     r"<div id='results_objectname\d+' style='z-index:1000;' onclick=''>.*?<a  href=\"(.*?)\"   >(?P<ime>.*?)<\/a>",
@@ -17,9 +18,17 @@ vzorec_za_igre = re.compile(
 
 
 url_naslovi = []
-for st in range(1,6):
-    vsebina = orodja.vsebina_datoteke("najigre_stran_{}".format(st))
+for st in range(1,11):
+    vsebina = orodja.vsebina_datoteke("najigre_stran_{}.html".format(st))
     url_naslovi += re.findall(vzorec_za_igre, vsebina)
     print(len(url_naslovi))
 orodja.zapisi_json(url_naslovi, "url_naslovi.json")
+'''
 
+with open("url_naslovi.json") as dat:
+    naslovi = json.load(dat)
+
+i = 0
+for naslov in naslovi:
+    i += 1
+    orodja.shrani_spletno_stran("https://boardgamegeek.com{}/credits".format(naslov[0]), "igra_{}.html".format(i))
